9e81f83b7de2ad381546e0cb75ab3819700edd67
なるみ <4680567+narumiruna@users.noreply.github.com>
2018-01-06 23:07:29 +0800

    Update mnist_classifier.py

--- examples/mnist_classifier.py
+++ examples/mnist_classifier.py
@@ -1,48 +1,47 @@
 import argparse
 
-# import matplotlib.pyplot as plt
 import numpy as np
-from time import time
 import torch
-import torch.nn.functional as F
 from torch import nn
 from torch.autograd import Variable
+from torch.utils import data
 from torchvision import datasets, transforms
-
-from utils import var_to_numpy
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--batch_size', type=int, default=128)
-parser.add_argument('--parallel', action='store_true')
+parser.add_argument('--cuda', action='store_true')
 args = parser.parse_args()
+
+args.cuda = args.cuda and torch.cuda.is_available()
+print(args)
+
 
 class Net(nn.Module):
     def __init__(self):
         super(Net, self).__init__()
         self.conv = nn.Sequential(
-            nn.Conv2d(1, 32, 5, 1, 2),
+            nn.Conv2d(1, 32, 5, 2),
             nn.BatchNorm2d(32),
             nn.ReLU(),
-            nn.MaxPool2d(2),
             nn.Dropout2d(),
-            nn.Conv2d(32, 64, 5, 1, 2),
+            nn.Conv2d(32, 64, 5, 2),
             nn.BatchNorm2d(64),
             nn.ReLU(),
-            nn.MaxPool2d(2),
             nn.Dropout2d()
         )
+
         self.linear = nn.Sequential(
-            nn.Linear(64*7*7, 1024),
+            nn.Linear(64 * 4 * 4, 1024),
             nn.BatchNorm2d(1024),
             nn.ReLU(),
             nn.Dropout(),
             nn.Linear(1024, 10),
-            nn.Sigmoid()
         )
 
     def forward(self, input_):
         output = self.conv(input_)
-        output = self.linear(output.view(-1, 64*7*7))
+        output = output.view(-1, 64 * 4 * 4)
+        output = self.linear(output)
         return output
 
 
@@ -51,72 +50,75 @@ transform = transforms.Compose([
     transforms.Normalize((0.13066047740239478,), (0.3081078087569972,))
 ])
 
-train_dataset = datasets.MNIST('../data', train=True, transform=transform, download=True)
-train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=args.batch_size, shuffle=True, num_workers=4)
+train_dataset = datasets.MNIST('../data',
+                               train=True,
+                               transform=transform,
+                               download=True)
+train_loader = data.DataLoader(train_dataset,
+                               batch_size=args.batch_size)
 
-test_dataset = datasets.MNIST('../data', train=False, transform=transform, download=True)
-test_loader = torch.utils.data.DataLoader(test_dataset, batch_size=args.batch_size, shuffle=True, num_workers=4)
-
+test_dataset = datasets.MNIST('../data',
+                              train=False,
+                              transform=transform,
+                              download=True)
+test_loader = data.DataLoader(test_dataset,
+                              batch_size=args.batch_size)
 
 net = Net()
-if args.parallel:
-    print('Use data parallel.')
-    net = nn.DataParallel(net)
-
-if torch.cuda.is_available():
-    print('Use cuda.')
+if args.cuda:
     net.cuda()
 
-
 optimizer = torch.optim.Adam(net.parameters(), lr=1e-3)
-losses = []
+cross_entropy = nn.CrossEntropyLoss()
 
-def train(epoch):
+
+def train():
     net.train()
 
-    for batch_idx, (batch_x, batch_y) in enumerate(train_loader):
-        batch_x, batch_y = Variable(batch_x), Variable(batch_y)
+    losses = []
 
-        if torch.cuda.is_available():
-            batch_x = batch_x.cuda()
-            batch_y = batch_y.cuda()
+    for train_index, (train_x, train_y) in enumerate(train_loader):
+        train_x = Variable(train_x)
+        train_y = Variable(train_y)
 
-        loss = F.cross_entropy(net(batch_x), batch_y)
+        if args.cuda:
+            train_x = train_x.cuda()
+            train_y = train_y.cuda()
 
-        optimizer.zero_grad()
+        loss = cross_entropy(net(train_x), train_y)
+
         loss.backward()
         optimizer.step()
+        optimizer.zero_grad()
 
-        losses.append(float(var_to_numpy(loss)))
+        losses.append(loss)
 
-
-    acc = evaluation()
-
-    print('Train epoch: {}, loss: {}, acc: {}'.format(epoch, var_to_numpy(loss), acc))
+    return torch.cat(losses)
 
 
-
-def evaluation():
+def evaluate():
     net.eval()
 
     correct = 0
-    for _, (batch_test_x , batch_test_y) in enumerate(test_loader):
-        batch_test_x, batch_test_y = Variable(batch_test_x, volatile=True), Variable(batch_test_y)
+    for _, (test_x, test_y) in enumerate(test_loader):
+        test_x = Variable(test_x, volatile=True)
+        test_y = Variable(test_y)
 
-        if torch.cuda.is_available():
-            batch_test_x = batch_test_x.cuda()
-            batch_test_y = batch_test_y.cuda()
+        if args.cuda:
+            test_x = test_x.cuda()
+            test_y = test_y.cuda()
 
-        correct += np.sum(var_to_numpy(batch_test_y) == np.argmax(var_to_numpy(net(batch_test_x)), axis=1))
+        _, max_indices = net(test_x).max(dim=1)
+        correct += int((max_indices == test_y).sum())
 
-    return correct / len(test_dataset)
+    return correct / len(test_loader.dataset)
 
-start = time()
+
 for epoch in range(10):
-    train(epoch)
-end = time()
+    losses = train()
+    train_avg_loss = float(losses.mean())
+    test_acc = evaluate()
 
-print('Time: {}'.format(end - start))
-
-#plt.plot(losses)
-#plt.show()
+    print('Train epoch: {}, train avg loss: {}, test acc: {}'.format(epoch,
+                                                                     train_avg_loss,
+                                                                     test_acc))
